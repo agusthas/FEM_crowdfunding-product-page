c28d0a5e6f9ff0d89d501c24d647ad9ffcb55ec4
agusthas <75809885+agusthas@users.noreply.github.com>
2021-06-06 06:51:11 +0700

    Header and Navbar (#1)
    * fix: move favicon to root
    
    * misc: adding rules ignore to stylelint
    
    * config: add base path for `vite`
    
    * feat: adding header & navbar (responsive),
    
    feat: added sass partials, variables

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
 import '../scss/index.scss';
+
+// Mobile Navbar
+
+// 1. Get all Elements
+const iconWrapper = document.querySelector('.js-icon-toggle');
+const nav = document.querySelector('.js-nav-toggle');
+const navBody = document.querySelector('.js-nav-body-toggle');
+
+// 2. Add Event and Its Handler
+iconWrapper.addEventListener('click', (e) => {
+  e.stopPropagation();
+
+  [...iconWrapper.children].forEach((node) => {
+    node.classList.toggle('is-hidden');
+  });
+
+  nav.classList.toggle('is-toggled');
+  navBody.classList.toggle('is-hidden');
+});
