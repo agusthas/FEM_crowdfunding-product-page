42bf8ed3c6f1a1897bd3288d89d8e9551e109e95
agusthas <suzumantan@gmail.com>
2021-06-07 13:05:52 +0700

    feat: add modal

--- src/js/index.js
+++ src/js/index.js
@@ -16,6 +16,7 @@ iconWrapper.addEventListener('click', (e) => {
   });
 
   nav.classList.toggle('is-toggled');
+  document.body.classList.toggle('has-hide-overflow');
   navBody.classList.toggle('is-hidden');
 });
 
@@ -34,4 +35,114 @@ bookmarkBtn.addEventListener('click', (e) => {
   } else {
     bookmarkText.dataset.text = 'Bookmark';
   }
+});
+
+// Modal
+
+// 1. Get all elements
+const modalOpenBtn = document.querySelectorAll('.js-modal-open');
+const modalCloseBtn = document.querySelector('.js-modal-close');
+const modalWrapper = document.querySelectorAll('.js-modal');
+// console.log(modalWrapper);
+
+modalOpenBtn.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    document.body.style.overflow = 'hidden';
+
+    modalWrapper.forEach((modal) => {
+      modal.classList.remove('is-hidden');
+    });
+  });
+});
+
+modalCloseBtn.addEventListener('click', (e) => {
+  document.body.style.overflow = 'auto';
+
+  modalWrapper.forEach((modal) => {
+    modal.classList.add('is-hidden');
+  });
+});
+
+// modalCloseBtn.forEach((btn) => {
+//   btn.addEventListener('click', (e) => {
+//     document.body.style.overflow = 'auto';
+
+//     modalWrapper.forEach((modal) => {
+//       modal.classList.add('is-hidden');
+//     });
+//   });
+// });
+
+// Modal Form Control
+
+// 1. Get all elements
+const formControl = document.querySelectorAll('.js-form-control');
+const radioBtns = document.querySelectorAll('.js-c-radio__input');
+const dropdowns = document.querySelectorAll('.js-form-control-dropdown');
+
+const resetDropdown = () => {
+  dropdowns.forEach((dd) => dd.classList.add('is-hidden-recurse'));
+};
+
+resetDropdown();
+radioBtns.forEach((radio) => {
+  const wrapper = radio.closest('.js-form-control');
+
+  const dropdown = wrapper.querySelector('.js-form-control-dropdown');
+
+  if (radio.checked) {
+    wrapper.classList.add('has-active');
+    dropdown.classList.remove('is-hidden-recurse');
+  }
+
+  radio.addEventListener('change', (e) => {
+    resetDropdown();
+    formControl.forEach((ctrl) => {
+      ctrl.classList.remove('has-active');
+    });
+
+    wrapper.classList.add('has-active');
+    dropdown.classList.remove('is-hidden-recurse');
+  });
+});
+
+// Form Submit
+
+// 1. Get all elements
+const forms = document.querySelectorAll('.js-form');
+
+// 2. Events
+forms.forEach((form) => {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const tier = e.target.closest('.c-form-control').dataset.tier;
+    let value = -1;
+    if (tier === 'free') {
+      modalCloseBtn.click();
+      return;
+    }
+
+    value = +e.target.querySelector('input[type="text"]').value;
+
+    if (tier === 'bamboo') {
+      if (value < 25) {
+        alert('Value of input must be higher than $25');
+      } else {
+        document.querySelectorAll('[data-stock-bamboo]').forEach((node) => {
+          node.innerHTML = +node.innerHTML - 1;
+        });
+        modalCloseBtn.click();
+      }
+    } else if (tier === 'black') {
+      if (value < 75) {
+        alert('Value of input must be higher than $75');
+      } else {
+        document.querySelectorAll('[data-stock-black]').forEach((node) => {
+          node.innerHTML = +node.innerHTML - 1;
+        });
+        modalCloseBtn.click();
+      }
+    }
+  });
 });
