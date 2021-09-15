2fa4dc812d52bef532735a5be2e6e62b937b1654
SusuMantan <suzumantan@gmail.com>
2021-09-15 13:35:08 +0700

    change url

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:agusthas/crowdfunding-product-page.git master:gh-pages
+git push -f git@github.com:agusthas/FEM_crowdfunding-product-page.git master:gh-pages
 
 cd -
